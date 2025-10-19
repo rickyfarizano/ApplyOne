1772e4ccec74ce9372807b24264171da9938d3a3
rickyfarizano <rickyfarizano@gmail.com>
2025-10-19 02:03:27 -0300

    feat:adding editJob endpoint

--- applyone-api/routes/web.php
+++ applyone-api/routes/web.php
@@ -7,6 +7,11 @@ Route::get('/', function () {
     return response()->json(['message' => 'Bienvenido a la API de ApplyOne']);
 });
 
+// Ruta para obtener token de validacion csrf
+Route::get('/csrf-token', function() {
+    return response()->json(['csrf_token' => csrf_token()]);
+});
+
 // Rutas de obtencion de trabajos
 Route::get('/jobs/get-all-jobs', [App\Http\Controllers\JobsController::class, 'getAllJobs']);
 Route::get('/jobs/get-job-by-id/{id}', [App\Http\Controllers\JobsController::class, 'getJobById']);
@@ -14,7 +19,6 @@ Route::get('/jobs/get-job-by-platform/{platformName}', [App\Http\Controllers\Job
 
 // Ruta de creacion de trabajos
 Route::post('/jobs/create-job', [App\Http\Controllers\JobsController::class, 'createJob']);
-
-Route::get('/csrf-token', function() {
-    return response()->json(['csrf_token' => csrf_token()]);
-});
+// Ruta de edicion de trabajos -> enviar campos a editar en json
+Route::patch('/jobs/edit-job/{id}', [App\Http\Controllers\JobsController::class, 'editJob']);
+// Ruta de eliminacion de trabajos
