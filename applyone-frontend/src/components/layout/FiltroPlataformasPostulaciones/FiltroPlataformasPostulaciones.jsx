10dac1529dc2594db7e354064fc8336496367097
rickyfarizano <rickyfarizano@gmail.com>
2025-10-20 14:59:33 -0300

    feat:adding platform btns into FiltroPLataformasPostulacion

--- applyone-frontend/src/components/layout/FiltroPlataformasPostulaciones/FiltroPlataformasPostulaciones.jsx
+++ applyone-frontend/src/components/layout/FiltroPlataformasPostulaciones/FiltroPlataformasPostulaciones.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import BtnPlatforma from '../../ui/BtnPlataforma/BtnPlatforma.jsx'
 import styles from './filtroPlataformasPostulaciones.module.css'
 import axios from 'axios'
 
@@ -22,7 +23,13 @@ const FiltroPlataformasPostulaciones = () => {
     <>
     <div className="filtro-plataformas-postulaciones">
         <div className="container">
-
+            {
+                platforms.map(platform => (
+                    <BtnPlatforma 
+                    platform_name={platform.platform_name} 
+                    key={platform.id} />
+                ))
+            }
         </div>
     </div>
     </>
---
+++ applyone-frontend/src/components/ui/BtnPlataforma/BtnPlatforma.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const BtnPlatforma = ({platform_name}) => {
+  return (
+    <>
+    <a href="#">{platform_name}</a>
+    </>
+  )
+}
+
+export default BtnPlatforma
