379f5ea8fe5c79a5feca5ea39f5f88e9d9d1e7a0
rickyfarizano <rickyfarizano@gmail.com>
2025-11-03 12:21:12 -0300

    feat: header app styles

--- applyone-frontend/src/components/layout/HeaderApp/HeaderApp.jsx
+++ applyone-frontend/src/components/layout/HeaderApp/HeaderApp.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from './headerApp.module.css'
 
 const HeaderApp = () => {
   return (
     <>
-    <header>
-        <div className="container">
-            <a className='btn_close_session' href="#"><i class="fa-solid fa-arrow-right-from-bracket"></i></a>
+    <header className={styles.header_app}>
+        <div className={styles.container}>
+            <a className={styles.btn_close_session} href="#"><i className="close_session_icon fa-solid fa-arrow-right-from-bracket"></i></a>
         </div>
     </header>
     </>
