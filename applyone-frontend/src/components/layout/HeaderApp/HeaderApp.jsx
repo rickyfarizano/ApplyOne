4c7914a9ba8373d1110ec6012bf4a496d428a33a
rickyfarizano <rickyfarizano@gmail.com>
2025-11-03 12:08:22 -0300

    feat:header structure

---
+++ applyone-frontend/src/components/layout/HeaderApp/HeaderApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderApp = () => {
+  return (
+    <>
+    <header>
+        <div className="container">
+            <a className='btn_close_session' href="#"><i class="fa-solid fa-arrow-right-from-bracket"></i></a>
+        </div>
+    </header>
+    </>
+  )
+}
+
+export default HeaderApp
