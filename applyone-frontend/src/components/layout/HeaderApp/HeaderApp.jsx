c203cd3e5f2648c63993432b2b37ee51738cda1b
rickyfarizano <rickyfarizano@gmail.com>
2025-11-16 11:22:54 -0300

    feat:adding link platform button

--- applyone-frontend/src/components/layout/HeaderApp/HeaderApp.jsx
+++ applyone-frontend/src/components/layout/HeaderApp/HeaderApp.jsx
@@ -6,6 +6,7 @@ const HeaderApp = () => {
     <>
     <header className={styles.header_app}>
         <div className={styles.container}>
+            <button className={styles.link_platform}>Vincular una plataforma <i className={`${styles.link_icon} fa-solid fa-link`}></i></button>
             <a className={styles.btn_close_session} href="#"><i className="close_session_icon fa-solid fa-arrow-right-from-bracket"></i></a>
         </div>
     </header>
